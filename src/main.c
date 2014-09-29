aab7be2f4516185a6db9033a3359e3d551c4110e
Philippe Helluy <helluy@math.unistra.fr>
2014-09-29 14:48:14 +0200

    test h20 1

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "macromesh.h"
 #include <stdio.h>
 #include <assert.h>
+#include <h20.h>
 
 int main(void) {
 
@@ -9,6 +10,16 @@ int main(void) {
   ReadMacroMesh(&m,"../geo/disque.msh");
   BuildConnectivity(&m);
   PrintMacroMesh(&m);
+
+  MacroMesh mc;
+
+  ReadMacroMesh(&mc,"../geo/cube.msh");
+  BuildConnectivity(&mc);
+  PrintMacroMesh(&mc);
+
+  // test the geometric transformation
+
+  double xref[3]={0.5_F,0.5_F,0.5_F};
 
   return 0;
 
