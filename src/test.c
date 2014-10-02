ae950c3dad99b15f3367a028914e05058b666ef8
Philippe Helluy <helluy@math.unistra.fr>
2014-10-02 21:18:15 +0200

    unit test of the model struct

--- src/test.c
+++ src/test.c
@@ -2,6 +2,7 @@
 #include "geometry.h"
 #include "interpolation.h"
 #include "test.h"
+#include "model.h"
 
 #include <stdio.h>
 #include <math.h>
@@ -338,3 +339,36 @@ void AffineMap(double* x){
   x[2]=newx[2];
 
 }
+
+int TestModel(void){
+
+  int test= (1==1);
+  // creation of a simple transport model
+  Model tr;
+  tr.m=1; // only one conservative variable
+  tr.NumFlux=TransportNumFlux;
+  tr.BoundaryFlux=TransportBoundaryFlux;
+  tr.InitData=TransportInitData;
+  tr.ImposedData=TransportImposedData;
+
+  double wL[tr.m];
+  double wR[tr.m];
+  double flux1[tr.m],flux2[tr.m];
+
+  double x[3]={1,1,2};
+  double t=0;
+  double vn[3]={1/sqrt(3),1/sqrt(3),-1/sqrt(3)};
+
+  tr.InitData(x,wR);
+  tr.NumFlux(wL,wR,vn,flux1);
+  printf("f %f \n",flux1[0]);
+  tr.BoundaryFlux(x,t,wL,vn,flux2);
+  printf("f %f \n",flux2[0]);
+
+  double err=fabs(flux2[0]-flux1[0]);
+
+  test=(err < 1e-8);
+
+  return test;
+
+};
