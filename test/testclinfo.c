16d7bfbfacdbe46c117f14b2074dfb0ae8c78265
ph <helluy@math.unistra.fr>
2014-11-05 15:35:20 +0100

    forgot a source file

---
+++ test/testclinfo.c
@@ -0,0 +1,44 @@
+#include "test.h"
+#include "schnaps.h"
+#include<stdio.h>
+#include <assert.h>
+#include <math.h>
+
+int main(void) {
+  
+  // unit tests
+    
+  int resu=TestCLInfo();
+	 
+  if (resu) printf("CLInfo test OK !\n");
+  else printf("CLInfo test failed !\n");
+
+  return !resu;
+} 
+
+
+
+
+int TestCLInfo(void){
+
+  int test = (1==1);
+
+  CLInfo cli;
+
+  InitCLInfo(&cli,0,0);
+  PrintCLInfo(&cli);
+
+  char prog[]="#pragma OPENCL EXTENSION cl_khr_fp64: enable \n"
+    "__kernel  \n "
+    "void testadd(__global double* a,__global double* b){ \n"
+    " int i = get_global_id(0); \n"
+    "  a[i] += b[i]; \n"
+    "}\n";
+
+  BuildKernels(&cli,prog);
+
+
+
+  return test;
+
+}
