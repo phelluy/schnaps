ffc5dfdc539386f25d39b18d5f5a6b685ae398ad
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2015-05-07 15:02:17 +0200

    Use command-line arguments in vtime.py

--- vtime.py
+++ vtime.py
@@ -5,6 +5,8 @@
 import time
 from subprocess import * # for popen, running processes
 import csv
+import getopt
+import sys # to check for file existence, etc.
 
 # Create an empty tsv file with comments
 def create_csv(filename, comments):
@@ -19,41 +21,54 @@ def append_to_csv(filename, data):
     a.writerows([data])
     fd.close()
 
-create_csv("cputime.csv", "#n\ttime(s)")
-create_csv("gputime.csv", "#n\ttime(s)")
 
-cmd0 = []
-cmd0.append("./testmanyv")
-cmd0.append("-t 1")
-nmax = 64
+def main(argv):
+    filename = "time.csv"
+    gpu = False
+    nmax = 64
+    
+    usage = "./vtime.py" \
+            " -g<0 or 1>: use GPU?\n" \
+            " -n<int>: max number of subcells in each direction\n" \
+            " -f<filename>: output filename\n" 
+    try:
+        opts, args = getopt.getopt(argv,"g:f:n:")
+    except getopt.GetoptError:
+        print usage
+    for opt, arg in opts:
+        if opt in ("-g"):
+            gpu = (arg == "True" or arg == "true" or arg == "1")
+        if opt in ("-f"):
+            filename = arg
+        if opt in ("-n"):
+            nmax = int(arg)
 
-n = 1
-while n <= nmax:
+    cmd0 = []
+    cmd0.append("./testmanyv")
+    cmd0.append("-t 1")
+    if(gpu):
+        cmd0.append("-g1")
+    else:
+        cmd0.append("-g0")
 
-    cmd = cmd0
-    cmd.append("-x"+str(n))
-    cmd.append("-y"+str(n))
-    cmd.append("-g0")
+    create_csv("time.csv", "#n\ttime(s)\tcommand: "+str(cmd0) +"\n")
 
-    ts = time.time()
-    p = Popen(cmd, stdout = PIPE, stderr = PIPE)
-    p.wait()
-    te = time.time()
-    elapsed_time = te - ts
-    print "cpu: n:" + str(n) + ", elapsed time: " + str(elapsed_time)
-    append_to_csv("cputime.csv", [n, elapsed_time])
 
-    cmd = cmd0
-    cmd.append("-x"+str(n))
-    cmd.append("-y"+str(n))
-    cmd.append("-g1")
+    n = 1
+    while n <= nmax:
+        cmd = cmd0
+        cmd.append("-x"+str(n))
+        cmd.append("-y"+str(n))
 
-    ts = time.time()
-    p = Popen(cmd, stdout = PIPE, stderr = PIPE)
-    p.wait()
-    te = time.time()
-    elapsed_time = te - ts
-    print "gpu: n:" + str(n) + ", elapsed time: " + str(elapsed_time)
-    append_to_csv("gputime.csv", [n, elapsed_time])
+        ts = time.time()
+        p = Popen(cmd, stdout = PIPE, stderr = PIPE)
+        p.wait()
+        te = time.time()
+        elapsed_time = te - ts
+        print "n:" + str(n) + ", elapsed time: " + str(elapsed_time)
+        append_to_csv(filename, [n, elapsed_time])
 
-    n *= 2
+        n *= 2
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
