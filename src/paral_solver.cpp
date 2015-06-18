97b962a914412b6d70a8f0d9686241c74ffba415
efranck21 <franck@m-franck.u-strasbg.fr>
2015-06-18 10:07:19 +0200

    modif parasolver

--- src/paral_solver.cpp
+++ src/paral_solver.cpp
@@ -4,10 +4,49 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include <paralution.hpp>
+
+using namespace paralution;
+
 void Solver_Paral_CG_Jacobi(LinearSolver* lsol){
 
-int i=0;
+  
+  init_paralution();
+  info_paralution();
+  
+LocalVector<double> RHS;
+LocalVector<double> Sol;
+LocalMatrix<double> mat;
 
-i=1;
+ Sol.Allocate(”Sol”,lsol->neq);
+ RHS.Allocate(”RHS”,lsol->neq);
+ 
+ switch(lsol->storage_type) {
+  case SKYLINE :
+    MatrixVector_Skyline_to_Paralution(lsol,mat,RHS);
+    break;
+
+  case CSR :
+    assert(1==2);
+    break;
+
+  default : 
+    assert(1==2);
+  }
+
+
+
+ switch(lsol->storage_type) {
+  case SKYLINE :
+    Vector_Paralution_to_Skyline(lsol,Sol);
+    break;
+    
+  case CSR :
+    assert(1==2);
+    break;
+
+  default : 
+    assert(1==2);
+  }
 
 }
--- src/paral_solver.hpp
+++ src/paral_solver.hpp
@@ -5,7 +5,12 @@
 #include "linearsolver.h"
 #include <stdbool.h>
 
+#include <paralution.hpp>
 
 void Solver_Paral_CG_Jacobi(LinearSolver* lsol);
+
+void MatrixVector_Skyline_to_Paralution(LinearSolver* lsol,LocalMatrix<double> mat,LocalVector<double> RHS);
+
+void Vector_Paralution_to_Skyline(LinearSolver* lsol,LocalVector<double> Sol);
 
 #endif
