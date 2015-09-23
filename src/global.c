8d77207b0cd2821f2b8887b7bab601f0cb7d4504
Malcolm Roberts <malcolm.i.w.roberts@gmail.com>
2015-09-23 11:10:32 +0200

    Set default OpenCL device/platform choices via cmake.

--- src/global.c
+++ src/global.c
@@ -1,4 +1,15 @@
 #include "global.h"
 
+// Set default platform choices via cmake, for example:
+// cmake -D_CL_PLATFORM=1 -D_CL_DEVICE=1 ../
+#ifndef _CL_PLATFORM
 int nplatform_cl = 0;
+#else
+int nplatform_cl = _CL_PLATFORM;
+#endif
+
+#ifndef _CL_DEVICE
 int ndevice_cl = 0;
+#else
+int ndevice_cl = _CL_DEVICE;
+#endif
